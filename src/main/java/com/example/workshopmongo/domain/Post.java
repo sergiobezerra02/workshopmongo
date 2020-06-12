65017127cc4710a474295574cd95053205e03899
Sergio Bezerra <sergiobezerra02@gmail.com>
2020-06-11 22:15:32 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/example/workshopmongo/domain/Post.java
+++ src/main/java/com/example/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.example.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.example.workshopmongo.dto.AuthorDTO;
+import com.example.workshopmongo.dto.CommentsDTO;
 
 @Document
 public class Post implements Serializable{
@@ -18,6 +21,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentsDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		super();
@@ -71,6 +76,14 @@ public class Post implements Serializable{
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentsDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentsDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -96,5 +109,7 @@ public class Post implements Serializable{
 			return false;
 		return true;
 	}
+
+	
 	
 }
---
+++ src/main/java/com/example/workshopmongo/dto/CommentsDTO.java
@@ -0,0 +1,50 @@
+package com.example.workshopmongo.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class CommentsDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String texto;
+	private Date date;
+	private AuthorDTO author;
+	
+	public CommentsDTO() {
+		super();
+	}
+
+	public CommentsDTO(String texto, Date date, AuthorDTO author) {
+		super();
+		this.texto = texto;
+		this.date = date;
+		this.author = author;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public AuthorDTO getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(AuthorDTO author) {
+		this.author = author;
+	}
+	
+	
+
+}
